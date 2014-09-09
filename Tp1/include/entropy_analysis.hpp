5bb30477ddfcfe5f5dc8a0d9715385f26a2a1406
sachakantor <sachakantor@gmail.com>
2014-09-09 13:46:21 -0300

    Applicacion que toma un archivo de entrada y calcula la cantidad de veces que aparece una IP como destino y como origen en los ARP. Faltaria descartar los requests Response de la cuenta de apariciones, calcular la probabilidad de ambas fuentes para cada IP y la entropia

--- Tp1/include/entropy_analysis.hpp
+++ Tp1/include/entropy_analysis.hpp
@@ -1,1 +1,11 @@
-#include <map>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<unordered_map>
+#include<tuple>
+#include<limits>
+
+typedef unsigned long src_counter;
+typedef unsigned long dst_counter;
+typedef float probability;
+typedef char ip[16];
--- Tp1/src/entropy_analysis.cpp
+++ Tp1/src/entropy_analysis.cpp
@@ -1,6 +1,44 @@
-#include <entropy_analysis.hpp>
+#include<entropy_analysis.hpp>
+
+using namespace std;
 
 int main(int argc, char* argv[]){
+	ofstream output_file;
+	ifstream input_file;
+	unsigned long long requests_count = 0;
+	int result = 0,entropy_src = 0, entropy_dst = 0;
+	ip ip_src,ip_dst;
+	unordered_map<string,std::pair<src_counter,dst_counter> > ip_appareances;
 
-	return 0;
+
+	if(argc != 3){
+		std::cout << "Se requieren 2 parametros: archivo_entrada archivo_salida" << std::endl;
+		result = 1;
+	} else {
+		input_file.open(argv[1],ios::in);
+		output_file.open(argv[2],ios::out);
+		
+		while(input_file.good()){
+			input_file.ignore(numeric_limits<streamsize>::max(),';'); //timestamp
+			input_file.ignore(numeric_limits<streamsize>::max(),';'); //type
+			input_file.getline(ip_src,16,';');
+			input_file.getline(ip_dst,16,'\n');
+
+			++get<0>(ip_appareances[ip_src]);
+			++get<1>(ip_appareances[ip_dst]);
+			
+			++requests_count;
+		}
+
+		cout << "IP: #SRC #DST" << endl;
+		for(auto it = ip_appareances.cbegin();it != ip_appareances.cend();++it){
+			cout << it->first << ": " << it->second.first << ' ' << it->second.second << endl;
+		}
+
+		output_file.close();
+		input_file.close();
+
+	}
+	
+	return result;
 }
