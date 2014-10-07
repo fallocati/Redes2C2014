488c80cd022d6a787ed5dd4ad4c6a75b0d8e0816
Sacha S. Kantor <sacha.kantor+bitbucket@gmail.com>
2014-10-07 18:45:55 -0300

    RastreaRutas listo en cuanto a envio sucesivo de paqutees TTL. Falta decidir que campos se guardaran en el csv y el timestamp.

--- Tp2/src/rastrearutas.cpp
+++ Tp2/src/rastrearutas.cpp
@@ -1,125 +1,101 @@
-#include <iostream>
-#include <string>
-#include <crafter.h>
+#include<iostream>
+#include<string>
+#include<crafter.h>
+#include<vector>
 
 #include<rastrearutas.hpp>
 using namespace std;
 using namespace Crafter;
 
-int main(int argc, char** args) {
+int main(int argc, char* argv[]) {
 
     int result = 0;
-    if(argc != 3) {
-        cout << "Uso: " << endl;
-        cout << args[0] << " [interface] [destino]" << endl;
+    if(argc < 3) {
+        cout << "Numero incorrecto de parametros. " << endl;
+        cout << argv[0] << " interface destino [max_hops] [pkgs_por_hop]" << endl;
+        cout << "Ejemplo: " << endl;
         result = 1;
 
     }else{
-    /* Set the interface */
-        string iface = args[1];
+        string iface = argv[1];
+        string dst_ip = argv[2];
 
-        /* ----------- Host and traceroute data --------- */
-
-        /* This is the IP we want to scan */
-        string scan_ip = args[2];
-
-        /*  Set the destination port for the TCP packets */
-        int port_number = 85;
-
-        /* Max hops, i.e. the max number of TTL on the packets */
         int max_hops = 30;
+        if(argc >= 4)
+            max_hops = std::stoi(argv[3],nullptr);
 
-        /* --------- Common data to all packets ---------- */
+        int pks_per_hop = 10;
+        if(argc >= 5)
+            pks_per_hop = std::stoi(argv[4],nullptr);
 
         /* Create an IP header */
         IP ip_header;
-
-        /* Set the Source and Destination IP address */
         ip_header.SetSourceIP(GetMyIP(iface));
-        ip_header.SetDestinationIP(scan_ip);
+        ip_header.SetDestinationIP(dst_ip);
 
-        IPOptionTraceroute trace;
-        trace.SetIDNumber(RNG16());
-        trace.SetOrigIP(GetMyIP(iface));
+        ICMP icmp_header;
+        icmp_header.SetType(ICMP::EchoRequest);
 
-        /* Create a TCP header */
-        TCP tcp_header;
-
-        tcp_header.SetDstPort(port_number);
-        tcp_header.SetFlags(TCP::SYN);
-        tcp_header.SetWindowsSize(5480);
-
-        /* ---------------------------------------------- */
-
-        /* Create a container to hold all the TCP packets */
-        vector<Packet*> tcp_packets;
-
+        vector<Packet*> pings_packets;
         /* Create a packet for each TTL */
-        for(int ttl = 1 ; ttl <= max_hops ; ttl++) {
-
-            /* Set a random ID for the IP header */
-            ip_header.SetIdentification(RNG16());
-
-            /* Set the TTL */
+        for(int ttl=0;ttl<=max_hops;++ttl) {
             ip_header.SetTTL(ttl);
-
-            /* Set a Random sequence number for TCP syn packet*/
-            tcp_header.SetSeqNumber(1);
-            tcp_header.SetSrcPort(RNG16());
-
-            /* Create a packet */
-            Packet* packet = new Packet(ip_header/tcp_header);
-
-            /* Push the packet into the container */
-            tcp_packets.push_back(packet);
+            for(int pk=0;pk<pks_per_hop;++pk){
+                ip_header.SetIdentification(RNG16()); //Set a random ID for the IP header
+                pings_packets.push_back(new Packet(ip_header/icmp_header));
+            }
         }
 
-        cout << "[@] Sending the TCP packets. Wait..." << endl;
+        for(auto it = pings_packets.cbegin();it<pings_packets.cend();++it){
+            (*it)->Print();
+            cout << endl;
+        }
 
-        /* Create a TCP packet container to hold the responses */
-        PacketContainer ret_packets(tcp_packets.size());
 
-        SendRecv(tcp_packets.begin(),tcp_packets.end(),ret_packets.begin(),iface,1.0,3,48);
+        cout << "ESOS SON LOS PAQUETES" << endl;
 
-        cout << "[@] SendRecv function returns :-) " << endl;
+        /*
+         * At this point, we have all the packets into the
+         * pings_packets container. Now we can Send 'Em All.
+         *
+         * 48 (nthreads) -> Number of threads for distributing the packets
+         *                  (tunable, the best value depends on your
+         *                   network an processor).
+         * 1 (timeout) -> Timeout in seconds for waiting an answer
+         * 3  (retry)    -> Number of times we send a packet until a response is received
+         */
+        vector<Packet*> pongs_packets(pings_packets.size());
+        cout << "[@] Sending the ICMP echoes. Wait..." << endl;
+        SendRecv(pings_packets.begin(),pings_packets.end(),pongs_packets.begin(),iface,1,3,48);
 
-        /* Now, check each answer */
-        vector<Packet*>::iterator it_pck;
-
-        int counter = 1;
-        for(it_pck = ret_packets.begin() ; it_pck < ret_packets.end() ; it_pck++) {
+        /*
+         * pongs_packets is a pointer to a PacketContainer with the same size
+         * of pings_packets (first argument). So, at this point, (after
+         * the SendRecv functions returns) we can iterate over each
+         * reply packet, if any.
+         */
+        for(auto it_pk_ptr = pongs_packets.begin();it_pk_ptr<pongs_packets.end();++it_pk_ptr) {
             /* Check if the pointer is not NULL */
-            Packet* reply_packet = (*it_pck);
-            if(reply_packet) {
-
-                /* Get the IP layer of the replied packet */
-                IP* ip_layer = reply_packet->GetLayer<IP>();
-
-                /* Get source IP of the packet */
-                string src_ip = ip_layer->GetSourceIP();
-                /* Print the Source IP */
-                cout << "[" << counter << "] " << src_ip << endl;
-
-                /* Check if the received packet was an ICMP message */
-                ICMP* icmp_layer = reply_packet->GetLayer<ICMP>();
-
-                /* If isn't an ICMP packet, break the loop. You can do additional checks. (TCP flags, ICMP type, etc...)*/
-                if(!icmp_layer) break;
-
-            } else
-                cout << "[" << counter << "] *.*.*.* " << endl;
-
-            /* Increment counter (= TTL) */
-            counter++;
+            if(*it_pk_ptr) {
+                /* Get the ICMP layer */
+                ICMP* icmp_layer = (*it_pk_ptr)->GetLayer<ICMP>();
+                if(icmp_layer->GetType() == ICMP::TimeExceeded || icmp_layer->GetType() == ICMP::EchoReply) {
+                            /* Get the IP layer of the replied packet */
+                            //IP* ip_layer = (*it_pk_ptr)->GetLayer<IP>();
+                            /* Print the Source IP */
+                            //cout << "[@] Host " << ip_layer->GetSourceIP() << " up." << endl;
+                            //counter++;
+                            (*it_pk_ptr)->Print();
+                            cout << endl;
+                }
+            }
         }
 
-        /* Delete the container with the PINGS packets */
-        for(it_pck = tcp_packets.begin() ; it_pck < tcp_packets.end() ; it_pck++)
-            delete (*it_pck);
+        for(auto it_pk_ptr = pings_packets.begin();it_pk_ptr<pings_packets.end();++it_pk_ptr)
+            delete (*it_pk_ptr);
 
-        /* Delete the container with the responses, if there is one (check the NULL pointer) */
-        for(it_pck = ret_packets.begin() ; it_pck < ret_packets.end() ; it_pck++)
-            delete (*it_pck);
+        for(auto it_pk_ptr = pongs_packets.begin();it_pk_ptr<pongs_packets.end();++it_pk_ptr)
+            delete (*it_pk_ptr);
     }
 
     return result;
