1710800464bb4e75a364abdbc35aac46c5e0546d
Sacha S. Kantor <sacha.kantor+bitbucket@gmail.com>
2014-10-12 18:39:52 -0300

    Salida a archivo csv hecha en formato: timestamp;ip1;rtt1;ip2;rtt2...;ipN;rttN

--- Tp2/src/rastrearutas.py
+++ Tp2/src/rastrearutas.py
@@ -4,8 +4,11 @@ import sys
 import os
 from scapy.all import *
 
-if len(sys.argv) != 2:
-    sys.exit('Usage: rastrearutas.py <remote host>')
+if len(sys.argv) != 3:
+    sys.exit('Usage: rastrearutas.py <remote host> <output file>')
+
+output_file = open(sys.argv[2],"a")
+output_file.write(time.strftime("%Y%m%d%H%M"))
 
 ttl = 1
 while True:
@@ -15,12 +18,17 @@ while True:
     if not (reply is None):
         rtt = (end-begin) * 1000
         if reply[ICMP].type == 11 and reply[ICMP].code == 0:
+            output_file.write(";%s;%s" %(reply.src,rtt))
             print ttl, '->', reply.src, ' ', rtt,'ms'
         elif reply[ICMP].type == 0:
+            output_file.write(";%s;%s\n" %(reply.src,rtt))
             print ttl, '->', reply.src, ' ', rtt,'ms'
             break
 
     else:
+        output_file.write(";*;*")
         print ttl, '-> *'
 
     ttl+=1
+
+output_file.close()
