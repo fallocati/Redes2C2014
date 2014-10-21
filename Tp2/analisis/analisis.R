792d5dc6ab99d32e311fbd862021bc45567966ac
F. Allocati <fede.allocati@gmail.com>
2014-10-20 22:05:49 -0300

    analisis con dibujado de mapa

--- Tp2/analisis/analisis.R
+++ Tp2/analisis/analisis.R
@@ -1,5 +1,7 @@
 require(ggplot2)
 require(plyr)
+require(rjson)
+require(maps)
 
 # Helper functions
 insertRow <- function(existingDF, newrow) {    
@@ -16,7 +18,7 @@ loadData <- function (filename) {
     raw <- read.csv(filename, header = F, sep=";", na.strings = "*", dec=".",
     allowEscapes = T, colClasses = as.vector(c("myDate", rep(c("character", "numeric"),
     ttls))), col.names = c(c("DateTime"), rbind(paste("IP_TTL_", 0:ttls, sep = ""), 
-    paste("RTT_TTL_", 0:ttls, sep = ""))))
+    paste("RTT_TTL_", 0:ttls, sep = ""))), nrows = 20)
     
     raw[, c(1, 4:(ncol(raw) - 2))]
 }
@@ -42,27 +44,55 @@ rttSd <- function (data) {
 }
 
 meltedData <- function (data) {
-    melted <- data.frame(ttl = integer(0), experiment = integer(0), ip = character(0),
-    rtt=numeric(0))
+    melted <- data.frame(ttl = integer(0), experiment = integer(0), name = character(0),
+    ip = character(0), rtt = numeric(0))
     
     for (experiment in 1:nrow(data)) {
         for (j in seq(2, ncol(data), 2)) {
             ttl <- j / 2
-            ip <- sprintf("Hop %02d - %s", ttl, data[experiment, j])
+            name <- sprintf("Hop %02d - %s", ttl, data[experiment, j])
+            ip <- data[experiment, j]
             rtt <- data[experiment, j + 1]
-            melted <- insertRow(melted, data.frame(ttl, experiment, ip, rtt))
+            melted <- insertRow(melted, data.frame(ttl, experiment, name, ip, rtt))
         }
     }
+    
+    melted$name <- as.character(melted$name)
+    melted$ip <- as.character(melted$ip)
     
     melted[melted$ttl > 0, ]
 }
 
-summariseData <- function (melted) {    
-    res <- ddply(melted, ~ip, summarise, mean = replace(mean(rtt, na.rm = T), 
-    is.nan(mean(rtt, na.rm = T)), NA), sd = replace(sd(rtt, na.rm = T),
-    is.nan(sd(rtt, na.rm = T)), NA), min = min(rtt), max = max(rtt))
+geolocateIps <- function(ip) {        
+    ret <- data.frame()
     
-    res[order(as.character(res$ip)), ]
+    for (i in 1:length(ip)) {
+        if (is.na(ip[i]))
+            next
+        
+        url <- paste(c("http://freegeoip.net/json/", ip[i]), collapse='')
+        
+        value <- data.frame(t(unlist(fromJSON(readLines(url, warn=FALSE)))), stringsAsFactors = F)
+                
+        ret <- rbind(ret, value)
+    }
+    
+    ret    
+}
+
+summariseData <- function (melted) {
+    geoData <<- geolocateIps(unique(melted$ip)) 
+    
+    res <- ddply(melted, ~name, summarise, ip = unique(ip), 
+    longitude = as.numeric(replace(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip), c("longitude")], nrow(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip),]) == 0, NA)),
+    latitude = as.numeric(replace(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip), c("latitude")], nrow(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip),]) == 0, NA)),
+    city = replace(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip), c("city")], nrow(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip),])  == 0, NA),
+    country = replace(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip), c("country_name")], nrow(geoData[!is.na(unique(ip)) & geoData$ip == unique(ip),]) == 0, NA),
+    mean = replace(mean(rtt, na.rm = T), is.nan(mean(rtt, na.rm = T)), NA),
+    sd = replace(sd(rtt, na.rm = T), is.nan(sd(rtt, na.rm = T)), NA),
+    min = min(rtt), max = max(rtt))
+    
+    res[order(as.character(res$name)), ]
 }
 
 plotAcumulated <- function (summarized, melted) {
@@ -71,5 +101,87 @@ plotAcumulated <- function (summarized, melted) {
     geom_point(shape = 21, size = 6, fill = "white") +
     geom_errorbar(width = 0.5, aes(ymin = mean - sd, ymax = mean + sd)) +
     #geom_smooth(aes(ymin = mean - sd, ymax = mean + sd), stat = "identity") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(title = 'RTT a cada Host Intermedio', x = "Hop", y = "RTT")
+}
+
+plotZscore <- function (zscore) {    
+    ggplot(data=zscore, aes(x=ip, y=prom),na.rm = TRUE) +    
+    geom_bar(stat="identity",position = "identity") + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    labs(title = 'ZScore de cada RTTi', x = "IP", y = "ZScore")     
+    #ggsave(file="zscore.pdf") 
+}
+
+plotMap <-  function(summarized) {    
+    #xlim <- c(-171.738281, -56.601563)
+    #ylim <- c(12.039321, 71.856229)
+    
+    data <- summarized[!is.na(summarized$longitude) & summarized$longitude != 0 & !is.na(summarized$latitude) & summarized$latitude != 0, c("longitude", "latitude")]
+            
+    map("world", col = "#f2f2f2", fill = TRUE, bg = "white", lwd=0.05)
+    points(x = data$longitude, y = data$latitude, col = 'red', cex=0.5, pch=19)    
+    
+    firstNonNa <- -1
+    
+    
+    for(i in 1:nrow(summarized)) {            
+        if (!is.na(summarized$longitude[i]) & summarized$longitude[i] != 0 & !is.na(summarized$latitude[i]) & summarized$latitude[i] != 0)
+        {
+            firstNonNa <- i
+            break
+        }
+    }
+    
+    if (firstNonNa == -1)
+        return
+        
+    wasNA <- FALSE
+    
+    # lat_d2 <- as.numeric(geo[13,5])
+    # lon_d2 <- as.numeric(geo[13,6])
+    # inter <- gcIntermediate(c(lon_o, lat_o), c(lon_d, lat_d), n=50, addStartEnd=TRUE)
+    # inter2 <- gcIntermediate(c(lon_d, lat_d), c(lon_d2, lat_d2), n=50, addStartEnd=TRUE)
+    # lines(inter)
+    # lines(inter2)
+    #inter <- gcIntermediate(c(lon_o, lat_o), c(lon_d, lat_d), n=50, addStartEnd=TRUE)
+    #lines(inter)
+    
+    lastLongitude <- summarized$longitude[firstNonNa]
+    lastLatitude <- summarized$latitude[firstNonNa]
+    
+    pallete <- topo.colors(nrow(data) + 1)
+    nextColor <- 1
+    
+    for (i in (firstNonNa + 1):nrow(summarized)) {
+        
+        currLongitude <- summarized$longitude[i]
+        currLatitude <- summarized$latitude[i]
+        
+        if (is.na(currLongitude) | is.na(currLatitude)) {
+            wasNA <- TRUE
+            next
+        }
+        
+        if (lastLongitude == currLongitude & lastLatitude == currLatitude) {
+            next
+        }
+        
+        greatCircle <- gcIntermediate(c(lastLongitude, lastLatitude), c(currLongitude, currLatitude), n=5000, addStartEnd=TRUE)
+                
+        if (wasNA) {
+            lines(greatCircle, lwd= 0.5, lty = 2)
+            #arrows(lastLongitude, lastLatitude, currLongitude, currLatitude, code=2, lwd= 0.5, length = 0.03, col = pallete[nextColor])
+        } else {
+            lines(greatCircle, lwd= 0.5, lty = 1)
+            #arrows(lastLongitude, lastLatitude, currLongitude, currLatitude, code=2, lwd= 0.5, length = 0.03, col = pallete[nextColor])
+        }
+        lastLongitude <- currLongitude
+        lastLatitude <- currLatitude
+        wasNA <- FALSE
+        nextColor <- nextColor + 1
+    }
+    
+     dev.print(pdf, file="filename.pdf");
+     dev.off()
 }
