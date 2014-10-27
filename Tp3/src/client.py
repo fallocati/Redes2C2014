f468e14f4f1f52f7cd6a743a99a62663c478f98d
Sacha S. Kantor <sacha.kantor+bitbucket@gmail.com>
2014-10-27 17:06:20 -0300

    Instancia de un ejemplo copiada y comenzada la adaptacion para transferir en una unica direccion todos los archivos de un directorio. De momento transfiere bien el primer archivo, pero falla al querer transferir el siguiente (la apertura del socket esta dentro de un for... hay que sacarla de ahi y reutilizarlo para todos los archivos).

---
+++ Tp3/src/client.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+##########################################################
+#                 Trabajo Práctico 3                     #
+#         Programación de protocolos end-to-end          #
+#                                                        #
+#              Teoría de las Comunicaciones              #
+#                       FCEN - UBA                       #
+#              Segundo cuatrimestre de 2014              #
+##########################################################
+
+
+from base import FileTransferBase
+
+
+class FileTransferClient(FileTransferBase):
+
+    def __init__(self):
+        FileTransferBase.__init__(self)
+        self.incoming_filename = 'filesToTransfer/thunder.jpg'
+        self.outgoing_filename = 'filesToTransfer/dwight.jpg'
+
+    def _connect_socket(self, sock):
+        sock.connect((self.server_ip, self.server_port), timeout=10)
+
+
+if __name__ == '__main__':
+    #FileTransferClient().transferReceive()
+    FileTransferClient().transfer()
