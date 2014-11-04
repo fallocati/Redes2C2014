8c6d4f468e7c1c5086bbd84e55c3a44165d28b3d
Sacha S. Kantor <sacha.kantor+bitbucket@gmail.com>
2014-11-04 18:50:01 -0300

    Sincronizacion del experimento

--- Tp3/src/serverFede.py
+++ Tp3/src/serverFede.py
@@ -3,7 +3,7 @@
 ##########################################################
 #                 Trabajo Práctico 3                     #
 #         Programación de protocolos end-to-end          #
-#                                                        # 
+#                                                        #
 #              Teoría de las Comunicaciones              #
 #                       FCEN - UBA                       #
 #              Segundo cuatrimestre de 2014              #
@@ -11,24 +11,24 @@
 
 
 try:
-    from ptc import Socket, SHUT_WR    
+    from ptc import Socket, SHUT_WR,ABORT
 except:
     import sys
     sys.path.append('../../')
-    from ptc import Socket, SHUT_WR
+    from ptc import Socket, SHUT_WR,ABORT
 
 import time
 
 CHUNK_SIZE = 500
-SERVER_IP = '127.0.0.1'
+SERVER_IP = '0.0.0.0'
 SERVER_PORT = 6677
-EXPERIMENT_TIME = 60
+EXPERIMENT_TIME = 5
 
-delays = [10, 25, 50, 100, 200]
-probs = [0, 10, 25, 50, 75]
+delays = [50]
+probs = [0,10,50]
 
 for delay in delays:
-    for prob in probs:      
+    for prob in probs:
     # Usar sockets PTC dentro de bloques with. Esto nos asegura que los recursos
     # subyacentes serán liberados de forma adecuada una vez que el socket ya no
     # se necesite.
@@ -37,7 +37,7 @@ for delay in delays:
             try:
                 # Ligar el socket a una interfaz local a través de la tupla (IP, PORT).
                 server_sock.bind((SERVER_IP, SERVER_PORT))
-            
+
                 # Pasar al estado LISTEN.
                 server_sock.listen()
 
@@ -47,13 +47,18 @@ for delay in delays:
                 # dará por vencido.
                 server_sock.accept(timeout=100)
                 #print 'accepted'
-                server_sock.shutdown(SHUT_WR)
+                #server_sock.shutdown(SHUT_WR)
 
                 start = time.time()
-                while server_sock.is_connected() and time.time() - start < EXPERIMENT_TIME:                  
-                    server_sock.recv(CHUNK_SIZE)
+                #while server_sock.is_connected() and time.time() - start < EXPERIMENT_TIME:
+                pkg = "2"
+                while (pkg.find("LACONCHADETUHERMANA") == -1):
+                    pkg = server_sock.recv(CHUNK_SIZE)
+
+                server_sock.send("LACONCHADETUHERMANA")
+                server_sock.close(ABORT)
 
                 #print 'experiment end'
-            except Exception,e: 
-                print str(e)  
+            except Exception,e:
+                print str(e)
                 pass
